5ffc22f4c39ca4f62af03e60dd214c490f16cd7e
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-09-06 23:46:00 +0530

    implement navigation rail view for landscape mode

--- src/platform/android/app/src/main/java/org/mgba_emu/mgba/MainActivity.kt
+++ src/platform/android/app/src/main/java/org/mgba_emu/mgba/MainActivity.kt
@@ -16,12 +16,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import androidx.transition.Slide
 import androidx.transition.TransitionManager
+import com.google.android.material.color.MaterialColors
+import com.google.android.material.navigation.NavigationBarView
 import org.mgba_emu.mgba.databinding.ActivityMainBinding
+import org.mgba_emu.mgba.utils.ThemeHelper
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,34 +36,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = ContextCompat.getColor(applicationContext, android.R.color.transparent)
-        window.navigationBarColor = ContextCompat.getColor(applicationContext, android.R.color.transparent)
+
+        window.statusBarColor =
+            ContextCompat.getColor(applicationContext, android.R.color.transparent)
+        window.navigationBarColor =
+            ContextCompat.getColor(applicationContext, android.R.color.transparent)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
+        (binding.navigationView as NavigationBarView).setupWithNavController(navController)
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val slideTransition = Slide(Gravity.BOTTOM).apply {
                 duration = 250
-                addTarget(binding.bottomNavigation)
+                addTarget(binding.navigationView)
             }
 
             TransitionManager.beginDelayedTransition(
-                binding.bottomNavigation.parent as ViewGroup,
+                binding.navigationView.parent as ViewGroup,
                 slideTransition
             )
 
             when (destination.id) {
                 R.id.searchLocationsFragment -> {
-                    binding.bottomNavigation.visibility = View.GONE
+                    binding.navigationView.visibility = View.GONE
                 }
+
                 R.id.biosManagerFragment -> {
-                    binding.bottomNavigation.visibility = View.GONE
+                    binding.navigationView.visibility = View.GONE
                 }
+
                 else -> {
-                    binding.bottomNavigation.visibility = View.VISIBLE
+                    binding.navigationView.visibility = View.VISIBLE
                 }
             }
+        }
+
+        binding.statusBarShade.setBackgroundColor(
+            ThemeHelper.getColorWithOpacity(
+                MaterialColors.getColor(
+                    binding.root,
+                    com.google.android.material.R.attr.colorSurface
+                ),
+                0.9f
+            )
+        )
+
+        binding.statusBarShade.visibility = View.VISIBLE
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            binding.root
+        ) { _: View, windowInsets: WindowInsetsCompat ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val mlpStatusShade = binding.statusBarShade.layoutParams as ViewGroup.MarginLayoutParams
+            mlpStatusShade.height = insets.top
+            binding.statusBarShade.layoutParams = mlpStatusShade
+
+            windowInsets
         }
     }
 }
